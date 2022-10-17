d80e30b5e9681b2dbd874e1062918c4f718be66b
Renuka <84368764+Jatla-Renuka@users.noreply.github.com>
2022-10-17 14:28:31 +0530

    one more print is added

--- src/rcse/Demo2.java
+++ src/rcse/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.print("welcome to Devops");
 		// TODO Auto-generated method stub
+		System.out.print("welcome to cse");
 
 	}
 
